85a205cf0a9e5358d8ddb4931088d778a6292ed5
haidt4 <haicon2321993@gmail.com>
2016-04-10 02:56:00 +0700

    calculate F4 in python

--- Python/KeyStroke.py
+++ Python/KeyStroke.py
@@ -1,7 +1,11 @@
+import pandas
+
 # read file
+words = pandas.DataFrame.from_csv('word_count.txt', index_col=None)
 chars = pandas.DataFrame.from_csv('1gram.txt', index_col=None)
 bigrams = pandas.DataFrame.from_csv('2gram.txt', index_col=None)
 data = pandas.DataFrame.from_csv('hien.txt', index_col=None)
+number_of_column = 26
 
 #calculate F1:
 data1 = data[data['KeyCode']>32]
@@ -26,11 +30,13 @@ F2 = bigrams['value']
 F2 = pandas.DataFrame(F2, columns=['value'])
 F2['Count'] = 0
 F2['Sum'] = 0
+F2 = F2.head(number_of_column)
 
 F3 = bigrams['value']
 F3 = pandas.DataFrame(F3, columns=['value'])
 F3['Count'] = 0
 F3['Sum'] = 0
+F3 = F3.head(number_of_column)
 
 for i in xrange(length-1):
 	str = list2[0][i] + list2[0][i+1]
@@ -52,11 +58,48 @@ F2.__delitem__('Sum')
 F3.__delitem__('Count')
 F3.__delitem__('Sum')
 
+F2.set_value(F2[F2['duration'].isnull()].index,'duration',0)
+F3.set_value(F3[F3['duration'].isnull()].index,'duration',0)
+
 F2.to_csv('F2.txt', index=False)
 F3.to_csv('F3.txt', index=False)
 
 #calculate F4
-data4['KeyCode'] = data['KeyCode'].map(chr)
+data4 = data['KeyCode']
+data4 = pandas.DataFrame(data4, columns=['KeyCode'])
+data4['KeyDown'] = data['KeyDown']
+data4['KeyUp'] = data['KeyUp']
+
+F4 = words['value']
+F4 = pandas.DataFrame(F4, columns=['value'])
+F4['Count'] = 0
+F4['Sum'] = 0
+F4 = F4.head(number_of_column)
 
 list4 = data4.values.T.tolist()
 length = len(list4[0])
+str = ''
+begin = 0
+finish = 0
+for i in xrange(length):
+	if (list4[0][i] >= 65 and list4[0][i] <= 90) or (list4[0][i] >= 97 and list4[0][i] <= 122):
+		if str != '':
+			begin = list4[1][i]
+		str = str + chr(list4[0][i])
+		finish = list4[2][i]
+	else:
+		str = str.lower()
+		if len(F4[F4['value']==str]) != 0:
+			index = F4[F4['value']==str].index[0]
+			valF4 = finish - begin
+			F4.set_value(index,'Count',F4[F4['value']==str]['Count'] + 1)
+			F4.set_value(index,'Sum',F4[F4['value']==str]['Sum'] + valF4)
+		str = ''
+		begin = 0
+		finish = 0
+
+F4['duration'] = F4['Sum']/F4['Count']
+F4.__delitem__('Count')
+F4.__delitem__('Sum')
+F4.set_value(F4[F4['duration'].isnull()].index,'duration',0)
+F4.to_csv('F4.txt', index=False)
