1d662630fa222cd165c626c30ae3104f7c7fdde2
congltse02543 <justforfun274@gmail.com>
2016-04-10 05:40:13 +0700

    Hot fix

--- ExtractMistakeFeature.py
+++ ExtractMistakeFeature.py
@@ -8,6 +8,7 @@ flag = False
 i = 0
 list0 = raw[0].values.tolist()
 myDict = {}
+misWords = [0] * len(list0)
 
 while i < len(list0):
     if list0[i] == backSpaceKeyCode:
@@ -25,13 +26,9 @@ while i < len(list0):
     else:
         count = 0
     i = i+1
-misDelWords = [None] * len(myDict)
-misReplWords = [None] * len(myDict)
-totalMistake = 0
+
 listValue = myDict.values()
 listKey = myDict.keys()
-totalMistake = sum(listValue)
-percentMistake = float(totalMistake)/len(list0)
 indexOfMiswordArray = 0;
 indexOdReplace = 0
 list1 = list0
@@ -41,7 +38,6 @@ for hihi in range(len(list1)):
 
 # get all delete record
 for iDict in range(len(myDict)):
-    mis = "";
     number = 0
     index = 0
     while number < listValue[iDict] and listKey[iDict] - index >0 :
@@ -49,35 +45,25 @@ for iDict in range(len(myDict)):
         if list1[listKey[iDict] - index] != None:
             number+=1
             if listKey[iDict] - index -1 >0 and list1[listKey[iDict] - index - 1] == shiftKeyCode:
-                mis = mis + "_"
-                mis = mis + str(shiftKeyCode)
+                misWords[listKey[iDict] - index - 1] =1
                 list1[listKey[iDict] - index - 1]  = None
-            mis = mis + "_"
-            mis = mis + str(list1[listKey[iDict] - index])
+            misWords[listKey[iDict] - index] = 1
             list1[listKey[iDict] - index] = None
-    misDelWords[indexOfMiswordArray] = mis
-    indexOfMiswordArray +=1
+
 
 # get all replace record
 for iDict in range(len(myDict)):
-    repl = "";
     number = 0
     index = 0
     while number < listValue[iDict] and listKey[iDict] + index < len(list0) :
         index +=1
         if list1[listKey[iDict] + index] != None:
             number+=1
-            repl = repl = "_"
-            repl = repl + str(list1[listKey[iDict] + index])
+            misWords[listKey[iDict] + index] = 1;
             if listKey[iDict] + index +1 <len(list0) and list1[listKey[iDict] + index] == shiftKeyCode:
-                repl = repl + "_"
-                repl = repl +str(list1[listKey[iDict] + index +1])
+                misWords[listKey[iDict] + index +1] =1;
                 list1[listKey[iDict] + index +1] = None
             list1[listKey[iDict] + index ] = None
-    misReplWords[indexOdReplace] = repl
-    indexOdReplace +=1
 
-DataFrame = pandas.DataFrame(misDelWords, index=None)
-DataFrame1 = pandas.DataFrame(misReplWords,index = None)
-result = pandas.concat([DataFrame, DataFrame1], axis=1)
+DataFrame = pandas.DataFrame(misWords, index=None)
 result.to_csv("featureMistakeWord.txt",index)
